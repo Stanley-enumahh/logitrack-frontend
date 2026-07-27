f0629d93b2fc8a23629386c59faed17659dccb21
Stanley enumah <enumah44@gmail.com>
2026-07-27 08:21:53 +0100

    track page

--- src/hooks/useGeolocation.ts
+++ src/hooks/useGeolocation.ts
@@ -14,8 +14,8 @@ export function getCurrentPosition(): Promise<{
     navigator.geolocation.getCurrentPosition(
       (position) => {
         resolve({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
+          latitude: Number(position.coords.latitude.toFixed(6)),
+          longitude: Number(position.coords.longitude.toFixed(6)),
         });
       },
       (error) => {
